84da4d57930695be678bbb640bb5dcf544e71e9d
tim <fitzgeraldt@gonzaga.edu>
2020-01-15 11:12:57 -0800

    Hello world example

---
+++ Julia/helloworld/Readme.md
@@ -0,0 +1,6 @@
+# Readme
+
+I run this file from the command line (or terminal).
+```
+$ julia ex1.jl
+```
---
+++ Julia/helloworld/ex1.jl
@@ -0,0 +1,10 @@
+# This is a comment in Julia code
+
+# This sets the enviroment (what packages I have to work with) to the local directory
+#using Pkg
+#Pkg.activate(".") # the dot means "here"
+
+# This loads the Printf package that gives me
+using Printf
+
+@printf("Hello world\n")
