4004e0a5bd7752bfd495a580c1ef9c897e9b81ae
tim <fitzgeraldt@gonzaga.edu>
2020-02-25 18:35:04 -0800

    in progress

--- class-demos/02-25/main1.m
+++ class-demos/02-25/main1.m
@@ -4,10 +4,25 @@ clear all
 close all
 clc
 
-%%
+%% System Parameters
 
-l1 = 
-l2 = 
-m1 = 
-m2 = 
-g = 
+l1 = 0.5; %[m]
+l2 = 0.5;%[m]
+m1 = 1; %[kg]
+m2 = 1; %[kg]
+g  = 9.81; %[m/s^2]
+
+
+%% Initial conditions
+theta1_0 = 45*pi/180; %[rad]
+theta2_0 = 60*pi/180; %[rad]
+
+theta1d_0 = 10; %[rad/s] 
+theta2d_0 = -10; %[rad/s]
+
+%%
+% Let's transform these coordinate-based IC to our states
+p1_0 = l1.^2.*m1.*theta1d_0 + m2.*(2*l1.^2.*theta1d_0 + 2*l1.*l2.*cos(theta1_0 - theta2_0).*theta2d_0)/2;
+p2_0 = m2.*(2*l1.*l2.*cos(theta1_0 - theta2_0).*theta1d_0 + 2*l2.^2.*theta2d_0)/2;
+
+
